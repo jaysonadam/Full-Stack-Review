6120ee1a23b352e49ab191d2a86a74e5d727b1ed
jaysonadam <jaysonadam@gmail.com>
2022-11-24 09:31:34 +0700

    Homework list complete

--- src/pages/school/homework/hw.js
+++ src/pages/school/homework/hw.js
@@ -6,6 +6,38 @@ import './hw.css';
 import moment from "moment";
 import { Card, Button, Offcanvas } from "react-bootstrap";
 
+function OffCanvas({info}) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const toggleShow = () => setShow((s) => !s);
+
+    return (
+        <>
+            <Button variant="dark" onClick={toggleShow} className="mt-auto">
+                See Details
+            </Button>
+            <Offcanvas show={show} onHide={handleClose} scroll="true" backdrop={true}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>{info.homework_name}</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                        {info.homework_desc}
+                    <br></br>
+                    <br></br>
+                        Submit through email.
+                    <br></br>
+                    <br></br>
+                        Due date : {moment(info.due_date).utc().format('YYYY-MM-DD')}
+                    <br></br>
+                    <br></br>
+                        <Button href="https://www.gmail.com">Submit</Button>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    )
+}
+
 function Homework() {
     const { id, fullname, role, stream_id } = useSelector((state) => state.auth);
     const [ homework, setHomework ] = useState([]);
@@ -49,40 +81,20 @@ function Homework() {
         }
     };
 
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const toggleShow = () => setShow((s) => !s);
-
     return (
         <>
             <h1>Homework</h1>
                 <div className="hw-menu">
                     <div class="d-flex flex-wrap col-9 my-5 justify-content-center">
-                        {homework.map((hw) => {
+                        {homework.map((hw, index) => {
+                            console.log(hw)
                             return (
-                                <Card className="hw-card">
+                                <Card key={index} className="hw-card">
                                     <h4>{hw.homework_name}</h4>
-                                    <Button variant="dark" onClick={toggleShow} className="mt-auto">
-                                        See Details
-                                    </Button>
-                                    <Offcanvas show={show} onHide={handleClose} scroll="true" backdrop={true}>
-                                        <Offcanvas.Header closeButton>
-                                            <Offcanvas.Title>{hw.homework_name}</Offcanvas.Title>
-                                        </Offcanvas.Header>
-                                        <Offcanvas.Body>
-                                            {hw.homework_desc}
-                                            <br></br>
-                                            <br></br>
-                                            Submit through email.
-                                            <br></br>
-                                            <br></br>
-                                            Due date : {moment(hw.due_date).utc().format('YYYY-MM-DD')}
-                                            <br></br>
-                                            <br></br>
-                                            <Button href="https://www.gmail.com">Submit</Button>
-                                        </Offcanvas.Body>
-                                    </Offcanvas>
+                                    <OffCanvas 
+                                        key={index}
+                                        info={hw}
+                                    />
                                 </Card>
                             )
                         })}
